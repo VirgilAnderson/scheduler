713fdb6639ec6dec3771120f432e08c3820daa96
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-10-11 20:38:35 -0600

    Feat: Basic calendar settings created

--- index.php
+++ index.php
@@ -7,6 +7,74 @@
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
+	<main>
+		<h1>Virgil's Scheduler</h1>
+		<div id='scheduler'>
+			<div id='schedule_head'>
+				<span>October</span>
+			</div>
+			<div id='schedule_body'>
+				<table>
+					<thead>
+						<th>Sun</th>
+						<th>Mon</th>
+						<th>Tue</th>
+						<th>Wed</th>
+						<th>Thu</th>
+						<th>Fri</th>
+						<th>Sat</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td>29</td>
+							<td>30</td>
+							<td>01</td>
+							<td>02</td>	
+							<td>03</td>	
+							<td>04</td>	
+							<td>05</td>	
+						</tr>
+						<tr>
+							<td>06</td>
+							<td>07</td>
+							<td>08</td>
+							<td>09</td>	
+							<td>10</td>	
+							<td>11</td>	
+							<td>12</td>	
+						</tr>
+						<tr>
+							<td>13</td>
+							<td>14</td>
+							<td>15</td>
+							<td>16</td>	
+							<td>17</td>	
+							<td>18</td>	
+							<td>19</td>	
+						</tr>
+						<tr>
+							<td>20</td>
+							<td>21</td>
+							<td>22</td>
+							<td>23</td>	
+							<td>24</td>	
+							<td>25</td>	
+							<td>26</td>	
+						</tr>
+						<tr>
+							<td>27</td>
+							<td>28</td>
+							<td>29</td>
+							<td>30</td>	
+							<td>31</td>	
+							<td>01</td>	
+							<td>02</td>	
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</main>
 	<script src="js/scheduler.js"></script>
 </body>
 </html>
