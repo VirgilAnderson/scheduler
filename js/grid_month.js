fea5b10e510ccf73abdc6345bab983727589fbd3
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-10-12 10:21:44 -0600

    Bug: Fixed display of next month error

--- js/grid_month.js
+++ js/grid_month.js
@@ -4,7 +4,7 @@ var schedule_head = document.getElementById('schedule_head');
 var schedule_body = document.getElementById('schedule_body');
 
 // Get today's date
-var today = new Date();
+var today = new Date(2019, 11, 1);
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
@@ -18,6 +18,7 @@ var first_day_of_the_month = days_of_week[first_date_of_the_month.getDay()];
 var days_last_month = first_date_of_the_month.getDay();
 var last_day_of_the_month = new Date(current_year, today.getMonth() + 1, 0);
 
+
 schedule_head.innerHTML = "<span>" + current_month + "</span>";
 
 var header_squares = 7;
@@ -28,18 +29,20 @@ var days = 1;
 
 for (var i = 0; i < header_squares; i++)
 {
+	// Print days headers
 	node += "<div class='day week_head " + days_of_week[i] +"'>" + days_of_week[i] + "</div>";
 }
 
 for(var i = 0; i < squares; i++)
 {
-	if (i < days_last_month || i > last_day_of_the_month.getDate() + 1)
+	// Only print days of current month
+	if (i < days_last_month || days > last_day_of_the_month.getDate())
 	{
 		node += "<div class='day'></div>";
 	}
 	else
 	{
-		if (days === today.getDay() - 1)
+		if (days === today.getDate())
 		{
 			node += "<div class='day today'>" + days + "</div>";
 		}
@@ -49,7 +52,6 @@ for(var i = 0; i < squares; i++)
 		}
 		days++;
 	}
-
 }
 
 schedule_body.innerHTML = node;
