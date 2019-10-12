1a87741c48d74d556580b1f28a126c7356c4b991
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-10-12 10:34:03 -0600

    Feat: Class begin

---
+++ scheduler_class_build.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scheduler Class Build</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/class_styles.css">
+</head>
+<body>
+	<main>
+		<h1>Virgil's Scheduler</h1>
+		<div id='scheduler'></div>
+		
+	</main>
+	<script src="js/class_scheduler.js"></script>
+</body>
+</html>
