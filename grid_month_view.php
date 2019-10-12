35d9ee0157572bd2d0567a22c3c4521b637c3b13
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-10-11 20:59:23 -0600

    Feat: added grid month view

---
+++ grid_month_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grid Month Scheduler</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/grid_month_styles.css">
+</head>
+<body>
+	<main>
+		<h1>Virgil's Scheduler</h1>
+		
+	</main>
+</body>
+</html>
