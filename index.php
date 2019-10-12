985c5598a2803e1d3728bd5ba6baa191d3ee3c76
Virgil Anderson <Virgil.Anderson@gmail.com>
2019-10-12 15:40:33 -0600

    Chore: Cleaned out old files

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Scheduler</title>
+	<title>Scheduler Class Build</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
@@ -9,71 +9,8 @@
 <body>
 	<main>
 		<h1>Virgil's Scheduler</h1>
-		<div id='scheduler'>
-			<div id='schedule_head'>
-				<span>October</span>
-			</div>
-			<div id='schedule_body'>
-				<table>
-					<thead>
-						<th>Sun</th>
-						<th>Mon</th>
-						<th>Tue</th>
-						<th>Wed</th>
-						<th>Thu</th>
-						<th>Fri</th>
-						<th>Sat</th>
-					</thead>
-					<tbody>
-						<tr>
-							<td class='muted'>29</td>
-							<td class='muted'>30</td>
-							<td>01</td>
-							<td>02</td>	
-							<td>03</td>	
-							<td>04</td>	
-							<td>05</td>	
-						</tr>
-						<tr>
-							<td>06</td>
-							<td>07</td>
-							<td>08</td>
-							<td>09</td>	
-							<td>10</td>	
-							<td class='today'>11</td>	
-							<td>12</td>	
-						</tr>
-						<tr>
-							<td>13</td>
-							<td>14</td>
-							<td>15</td>
-							<td>16</td>	
-							<td>17</td>	
-							<td>18</td>	
-							<td>19</td>	
-						</tr>
-						<tr>
-							<td>20</td>
-							<td>21</td>
-							<td>22</td>
-							<td>23</td>	
-							<td>24</td>	
-							<td>25</td>	
-							<td>26</td>	
-						</tr>
-						<tr>
-							<td>27</td>
-							<td>28</td>
-							<td>29</td>
-							<td>30</td>	
-							<td>31</td>	
-							<td class='muted'>01</td>	
-							<td class='muted'>02</td>	
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
+		<div id='schedule_container'></div>
+		
 	</main>
 	<script src="js/scheduler.js"></script>
 </body>
